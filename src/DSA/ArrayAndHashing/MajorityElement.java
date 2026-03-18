e65ccfb99d2ad9cf8b202c101afe1065c9b2f349
seshathri <m.seshathri.code@gmail.com>
2026-03-18 12:08:36 +0530

    Day 2:MajorityElement problem solved

---
+++ src/DSA/ArrayAndHashing/MajorityElement.java
@@ -0,0 +1,24 @@
+package DSA.ArrayAndHashing;
+
+public class MajorityElement {
+    public int Major(int [] arr){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count>n/2){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String [] args){
+        MajorityElement m = new MajorityElement();
+        int [] nums = {1,3,3,3,4};
+        System.out.println(m.Major(nums));
+    }
+}
